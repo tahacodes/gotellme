750a4b6c0df6077dcc72217735c9cd28bc430832
Taha Farahani <iotahacodes@gmail.com>
2020-06-30 23:04:00 +0430

    CNAME, IP Addresses, MX records and Name Servers

---
+++ cmd/cli/cli.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Simple website lookup tool"
+	app.Usage = "IP, CNAME, NameServers, MX records"
+
+	appFlags := []cli.Flag{
+		&cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
+	app.Commands = []*cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Looks up the Name Servers for a particular host",
+			Flags: appFlags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "ip",
+			Usage: "Looks up the IP addresses for a particular host",
+			Flags: appFlags,
+			Action: func(c *cli.Context) error {
+				ip, err := net.LookupIP(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(ip); i++ {
+					fmt.Println(ip[i])
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "cname",
+			Usage: "Looks up the CNAME for a particular host",
+			Flags: appFlags,
+			Action: func(c *cli.Context) error {
+				cname, err := net.LookupCNAME(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				fmt.Println(cname)
+				return nil
+			},
+		},
+		{
+			Name:  "mx",
+			Usage: "Looks up the MX records for a particular host",
+			Flags: appFlags,
+			Action: func(c *cli.Context) error {
+				mx, err := net.LookupMX(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(mx); i++ {
+					fmt.Println(mx[i].Host, mx[i].Pref)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
